1687c42fc40e741d3dad966fdf6e5596f05828f5
tchan70 <toby70@hotmail.co.uk>
2018-10-23 11:35:00 +0100

    now can do all basic operations

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 console.log('Welcome to the caculator!');
 const readline = require('readline-sync');
-console.log('Please enter some numbers:');
+console.log('Please enter your operator:');
+const operator = readline.prompt();
 console.log('Please enter your first number')
 const firstargument = readline.prompt();
 const firstnumber = +firstargument;
 console.log('Please enter your second number')
 const secondargument = readline.prompt();
 const secondnumber = +secondargument;
-const answer = firstnumber * secondnumber;
+let answer = 0;
+if (operator === '+') {
+    answer = firstnumber + secondnumber
+}else if (operator === '-') {
+    answer = firstnumber - secondnumber
+}else if (operator === '*') {
+    answer = firstnumber * secondnumber
+}else if (operator === '/') {
+    answer = firstnumber / secondnumber
+}
 console.log('Here is the answer ' +answer);

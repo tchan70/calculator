920ac0ec6862adbc3252442396c889643aa72328
tchan70 <toby70@hotmail.co.uk>
2018-10-23 12:09:13 +0100

    calculator can now do stuff with multiple numbers

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ console.log('Please only use numbers and the basic computer operators, no fancy 
 const readline = require('readline-sync');
 console.log('Please enter your operator:');
 const operator = readline.prompt();
-console.log('Please enter your first number')
-const firstargument = readline.prompt();
-const firstnumber = +firstargument;
-console.log('Please enter your second number')
-const secondargument = readline.prompt();
-const secondnumber = +secondargument;
-let answer = 0;
+console.log('Please enter the number of numbers you want to ' +operator +'?');
+const numberofarguments = readline.prompt();
+const numberofnumbers = +numberofarguments
+let numbers = new Array(numberofnumbers);
+for (let egg = 0; egg < numberofnumbers; egg++) {
+    console.log('Please enter number ' + (egg + 1) + ':');
+    const argument = readline.prompt();
+    numbers[egg] = +argument;
+}
+let answer = numbers[0];
+for (let egg = 1; egg < numberofnumbers; egg++){
 if (operator === '+') {
-    answer = firstnumber + secondnumber
+    answer += numbers[egg]
 }else if (operator === '-') {
-    answer = firstnumber - secondnumber
+    answer -= numbers[egg]
 }else if (operator === '*') {
-    answer = firstnumber * secondnumber
+    answer *= numbers[egg]
 }else if (operator === '/') {
-    answer = firstnumber / secondnumber
+    answer /= numbers[egg]
+}
 }
 console.log('Here is the answer ' +answer);

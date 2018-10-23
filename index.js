0872e1cecd40e6ddd92e6af726434ae5394d8e6e
tchan70 <toby70@hotmail.co.uk>
2018-10-23 15:21:44 +0100

    continuous working calculator

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-console.log('Welcome to the caculator!');
-console.log('Please only use numbers and the basic computer operators, no fancy ones.')
 const readline = require('readline-sync');
-console.log('Please enter your operator:');
-const operator = readline.prompt();
-console.log('Please enter the number of numbers you want to ' +operator +'?');
-const numberofarguments = readline.prompt();
-const numberofnumbers = +numberofarguments
-let numbers = new Array(numberofnumbers);
-for (let egg = 0; egg < numberofnumbers; egg++) {
-    console.log('Please enter number ' + (egg + 1) + ':');
-    const argument = readline.prompt();
-    numbers[egg] = +argument;
+
+function printWelcomeMessage() {
+    console.log('Welcome to the caculator!');
+    console.log('Please only use numbers and the basic computer operators, no fancy ones.');
+    console.log('=========================================================================');
 }
-let answer = numbers[0];
-for (let egg = 1; egg < numberofnumbers; egg++){
-if (operator === '+') {
-    answer += numbers[egg]
-}else if (operator === '-') {
-    answer -= numbers[egg]
-}else if (operator === '*') {
-    answer *= numbers[egg]
-}else if (operator === '/') {
-    answer /= numbers[egg]
+
+function getStringInputWithPrompt(prompt) {
+    console.log(prompt);
+    return readline.prompt();    
 }
+
+function getNumberInputWithPrompt(prompt) {
+    let response;
+    do {
+        response = +getStringInputWithPrompt(prompt);
+    }while (isNaN(response));
+    return response;
 }
-console.log('Here is the answer ' +answer);
+
+function getOperator() {
+    return getStringInputWithPrompt('Please enter the operator you would like to use:');
+}
+
+function getNumberArray(operator) {
+    const iterations = getNumberInputWithPrompt('Please enter the number of numbers you want to ' + operator + '?');
+    let numbers = new Array(iterations);
+    for (let egg = 0; egg < iterations; egg++) {
+        numbers[egg] = getNumberInputWithPrompt('Please enter number ' + (egg + 1) + ':');
+    }
+    return numbers;
+}
+
+function calculateAnswer(operator, numbers) {
+    let answer = numbers[0];
+    for (let egg = 1; egg < numbers.length; egg++){
+        if (operator === '+') {
+           answer += numbers[egg];
+        }else if (operator === '-') {
+           answer -= numbers[egg];
+        }else if (operator === '*') {
+           answer *= numbers[egg];
+        }else if (operator === '/') {
+           answer /= numbers[egg];
+        }
+    }
+    return answer;
+}
+
+function performOneCalculation() {
+    var operator = getOperator();
+    var numbers = getNumberArray(operator);
+    var answer = calculateAnswer(operator, numbers);
+
+    console.log('Here is the answer ' +answer);
+}
+
+printWelcomeMessage();
+while (true) {
+    performOneCalculation();
+}
